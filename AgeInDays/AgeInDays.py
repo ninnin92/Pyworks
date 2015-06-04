26aed0b0251e6966d6cf57bb0fa475e08d3a4a98
ninnin92 <ninnin92.28@gmail.com>
2015-06-04 16:48:42 +0900

    子どもの年齢・月齢・日齢計算： Python2 ,3 どちらにも対応

--- AgeInDays/AgeInDays.py
+++ AgeInDays/AgeInDays.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # coding:utf-8
+# env: Python3.3
+# python2系でも動くように調整済み
 
 from datetime import date, timedelta, datetime
 
 
+# y年における誕生日（閏日補正含む）
 def count_years(b, t):
-    # y年における誕生日（閏日補正含む）
     try:
         this_year = b.replace(year=t.year)
     except ValueError:
@@ -19,6 +21,7 @@ def count_years(b, t):
     return age
 
 
+# 月齢の計算
 def count_months(b, t):
     if (t.day - b.day) >= 0:
         months = (t.year - b.year)*12 + (t.month - b.month)
@@ -27,6 +30,13 @@ def count_months(b, t):
     return months
 
 
+# 何歳”何ヶ月”を計算
+def count_year_months(b, t):
+    months = t.month - b.month  # 月の引き算
+    return months
+
+
+# 月齢および何歳何ヶ月の余り日数（何歳何ヶ月”何日”）
 def count_days(b, t):
     if (t.day - b.day) >= 0:
         days = t.day - b.day
@@ -36,18 +46,35 @@ def count_days(b, t):
     return days
 
 
-# 日付設定
+# 日付設定(今日)
 today = date.today()
-# birthday = date(1992, 2, 28)
-bd = input("Please enter your birthday   ")  # python2ではraw_input
+
+# 入力待ち(exceptでPython2系にも対応)
+try:
+    bd = input('Please enter your birthday (e.g. "2015/06/04")  ')
+except:
+    def input(text):
+        return raw_input(text)  # python2ではraw_input
+    bd = input('Please enter your birthday (e.g. "2015/06/04")  ')
+    pass
+
+# 誕生日をdatetime型に設定
 bd = datetime.strptime(bd, "%Y/%m/%d")
 birthday = date(bd.year, bd.month, bd.day)
 
 # 年齢を表示
-age_in_days = (today - birthday).days
-age = count_years(birthday, today)
-age_months = count_months(birthday, today)
-age_days = count_days(birthday, today)
+age = str(count_years(birthday, today))
+age_months = str(count_months(birthday, today))
+age_in_days = str((today - birthday).days)
+age_year_months = str(count_year_months(birthday, today))
+age_days = str(count_days(birthday, today))
+
+age_details = age + "years " + age_year_months + "months " + age_days + "days"
 
 
-print (age_in_days, age, age_months, age_days)  # python2では()なし
+print ("********************************************")  # python2では()なし
+print ("Age  " + age)
+print ("Age_months  " + age_months)
+print ("Age_in_days  " + age_in_days)
+print ("Age_details  " + age_details)
+print ("********************************************")
