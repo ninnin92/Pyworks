73f0075900ff15e0bb955f312b6ed65797bd950e
ninnin92 <ninnin92.28@gmail.com>
2015-06-17 13:16:51 +0900

    データ自動整理：自動整理-batファイル

---
+++ auto_analysis/Run_organize.bat
@@ -0,0 +1,21 @@
+set PYTHONPATH=%cd%
+echo Run %date% %time% > output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
+
+echo; >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
+python copy_from_site_to_files.py >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
+
+
+echo; >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
+python organize_for_summary.py >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
+
+
+echo; >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
+python organize_for_time-series.py >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
+
+
+echo; >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
+python ready_for_R.py >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
+
+
+echo; >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
+echo End %date% %time% >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_organize.txt
