b55ead73a63128dd55d75ac8e8b3a6c7b70423d3
ninnin92 <ninnin92.28@gmail.com>
2015-06-28 11:32:34 +0900

    kivyアプリ：ログ出力、直前の出力表示、UI調整

--- log-time/main.py
+++ log-time/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding:utf-8
 
+from __future__ import division
 from kivy.config import Config
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -20,21 +21,43 @@ class MyApp(App):
     data = []
     title = 'Log_Time'
 
+    app_time = "{0:%y-%m-%d_%H-%M}".format(dt.datetime.now())
+    fp = open("Log/" + "Log_" + app_time + ".csv", "w")
+
+    def bt_release(self):
+        self.root.hit1.color = [0.2, 0.70588, 0.56470, 1]
+        self.root.hit2.color = [0.2, 0.70588, 0.56470, 1]
+        self.root.hit3.color = [0.2, 0.70588, 0.56470, 1]
+
     def time_get(self, bt):
         now = "{0:%y/%m/%d %H:%M:%S}".format(dt.datetime.now())
         self.data.append([bt] + [now])
-        self.root.timel.text = bt + " --- " + now
-        print self.root.timel.text
+        self.root.timel3.text = self.root.timel2.text
+        self.root.timel2.text = self.root.timel1.text
+        self.root.timel1.text = "*  " + bt + " | " + now
+
+        self.root.hit1.color = [0.85, 0.85, 0.85, 1]
+        self.root.hit2.color = [0.85, 0.85, 0.85, 1]
+        self.root.hit3.color = [0.85, 0.85, 0.85, 1]
+
+        print (self.root.timel1.text)
 
     def build(self):
         return logtime()
 
     def on_start(self):
+        index = ["Event", "Time"]
+        for i in index:
+            self.fp.write("%s," % i)
+        self.fp.write("\n")
         print ("Start!")
 
     def on_stop(self):
-        print ("Stop!")
         print (self.data)
+        for x in self.data:
+            self.fp.write("%s, %s\n" % tuple(x))
+        self.fp.flush()
+        print ("Stop!")
 
     def on_pause(self):
         return True
