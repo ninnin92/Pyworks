732d2a224ef7553180a3102bdba1d03a7bb3c0e6
ninnin92 <ninnin92.28@gmail.com>
2015-06-29 14:07:24 +0900

    kivyアプリ - eventの時刻記録：UI調整

--- log-time/main.py
+++ log-time/main.py
@@ -7,9 +7,8 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 import datetime as dt
 
-Config.set('graphics', 'width', '400')
-Config.set('graphics', 'height', '640')
-# Config.set('graphics', 'fullscreen', 'auto')  # フルスクリーン
+Config.set('graphics', 'width', '800')
+Config.set('graphics', 'height', '800')
 Config.set('graphics', 'show_cursor', '1')
 
 
@@ -18,27 +17,40 @@ class logtime(Widget):
 
 
 class MyApp(App):
-    data = []
     title = 'Log_Time'
+    bt1 = "event1"
+    bt2 = "event2"
+    bt3 = "event3"
+
+    data = []
 
     app_time = "{0:%y-%m-%d_%H-%M}".format(dt.datetime.now())
     fp = open("Log/" + "Log_" + app_time + ".csv", "w")
 
-    def bt_release(self):
-        self.root.hit1.color = [0.2, 0.70588, 0.56470, 1]
-        self.root.hit2.color = [0.2, 0.70588, 0.56470, 1]
-        self.root.hit3.color = [0.2, 0.70588, 0.56470, 1]
+    def bt_release(self, bt):
+        if bt == 1:
+            self.root.hit1.color = [0.2, 0.70588, 0.56470, 1]
+        elif bt == 2:
+            self.root.hit2.color = [0.2, 0.70588, 0.56470, 1]
+        else:
+            self.root.hit3.color = [0.2, 0.70588, 0.56470, 1]
 
     def time_get(self, bt):
+        if bt == 1:
+            self.root.hit1.color = [0.8, 0.8, 0.8, 1]
+            bt = self.bt1
+        elif bt == 2:
+            self.root.hit2.color = [0.8, 0.8, 0.8, 1]
+            bt = self.bt2
+        else:
+            self.root.hit3.color = [0.8, 0.8, 0.8, 1]
+            bt = self.bt3
+
         now = "{0:%y/%m/%d %H:%M:%S}".format(dt.datetime.now())
         self.data.append([bt] + [now])
         self.root.timel3.text = self.root.timel2.text
         self.root.timel2.text = self.root.timel1.text
         self.root.timel1.text = "*  " + bt + " | " + now
-
-        self.root.hit1.color = [0.85, 0.85, 0.85, 1]
-        self.root.hit2.color = [0.85, 0.85, 0.85, 1]
-        self.root.hit3.color = [0.85, 0.85, 0.85, 1]
 
         print (self.root.timel1.text)
 
