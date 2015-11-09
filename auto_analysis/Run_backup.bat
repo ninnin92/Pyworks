357d84bcb816ad08b93c19a45be9dcdc18285356
ninnin92 <ninnin92.28@gmail.com>
2015-11-09 13:46:56 +0900

    2015/11/09 細かい修正

--- auto_analysis/Run_backup.bat
+++ auto_analysis/Run_backup.bat
@@ -2,11 +2,11 @@ set PYTHONPATH=%cd%
 echo Run %date% %time% > output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_backup.txt
 
 echo; >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_backup.txt
-python backup_for_Log.py >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_backup.txt
+python backup_for_Log.py >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_backup.txt 2>&1
 
 
 echo; >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_backup.txt
-python backup_for_Anlyz.py >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_backup.txt
+python backup_for_Anlyz.py >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_backup.txt 2>&1
 
 
 echo; >> output/%date:~-10,4%%date:~-5,2%%date:~-2,2%_backup.txt
